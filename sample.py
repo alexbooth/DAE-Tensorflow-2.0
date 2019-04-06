772a879b90a4e5c83a097fc0e4e4bd94f48a40e2
Alexander Booth <alexanderbooth@alumni.ubc.ca>
2019-04-06 13:41:54 -0700

    updated sample.py

--- sample.py
+++ sample.py
@@ -8,7 +8,7 @@ import numpy as np
 import tensorflow as tf
 from data_manager import DataManager
 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
 from model import Encoder, Decoder
 
@@ -16,12 +16,14 @@ from absl import app
 from absl import flags
 
 flags.DEFINE_integer("sample_size", 10, "samples to test")
-flags.DEFINE_string("model", "./trained_model/DAE_model.h5", "todo")
+flags.DEFINE_string("model", None, "Path to a trained model (.h5 file)")
 flags.DEFINE_boolean("use_noise", False, "sample noisey images")
 FLAGS = flags.FLAGS
 
-def sample(model, manager, n_samples):
+
+def sample(model, n_samples):
     """Passes n random samples through the model and displays X & X_pred"""
+    manager = DataManager()
     _, X = manager.get_batch(n_samples, use_noise=FLAGS.use_noise)
     X_pred = model.predict(X)
     x_dim, y_dim = X[0].shape[0], X[0].shape[1]
@@ -60,23 +62,28 @@ def load_model():
     found_decoder_weights = False
     decoder_layer_cnt = 0
     for i, layer in enumerate(model.layers):
-        if not found_decoder_weights: 
-            found_decoder_weights = ("decoder_input" == layer.name)
-        else:
-            decoder.layers[decoder_layer_cnt] = layer
+        print(layer.name)
+        weights = layer.get_weights()
+        if len(layer.get_weights()) > 0:
+            print(weights[0].shape, weights[1].shape)
+        if "decoder_input" == layer.name:
+            found_decoder_weights = True
+        if found_decoder_weights:
             decoder_layer_cnt += 1
+            print("dec:" + decoder.layers[decoder_layer_cnt].name)
+            decoder.layers[decoder_layer_cnt].set_weights(weights)
 
-    #encoder.compile()
-    #decoder.compile()
     encoder.summary()
     decoder.summary()
 
-    return model
-
+    return encoder, decoder, model
+       
 def main(argv):
-    manager = DataManager()
-    model = load_model()
-    #sample(model, manager, FLAGS.sample_size)
+    if FLAGS.model == None:
+        print("Please specify a path to a model with the --model flag")
+        sys.exit()
+    encoder, decoder, autoencoder = load_model()
+    sample(autoencoder, FLAGS.sample_size)
 
 if __name__ == '__main__':
     app.run(main)
